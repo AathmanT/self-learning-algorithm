957d340ea2372166c879f8d19a239ec2e1c71f8b
Vivek Vinushanth <cvivekvinushanth@gmail.com>
2020-02-16 16:37:22 +0530

    Modified display.py

--- gsom/applications/zoo_experiment/data_parser.py
+++ gsom/applications/zoo_experiment/data_parser.py
@@ -1,36 +1,36 @@
 import pandas as pd
 import re
-from keras.datasets import mnist
+# from keras.datasets import mnist
 
 
 class InputParser:
 
-    @staticmethod
-    def parse_input_zoo_data(filename, header='infer'):
+     @staticmethod
+     def parse_input_zoo_data(filename, header='infer'):
+    #
+    #     (X_train, y_train), (X_test, y_test) = mnist.load_data()
+    #
+    #     d1, d2, d3 = X_train.shape
+    #     X_train_reshaped = X_train.reshape(d1, d2 * d3)
+    #     print(X_train_reshaped[:2000, :].shape)
+    #     y_train_half = y_train[:2000]
+    #     classes = y_train_half.tolist()
+    #     labels = y_train_half.tolist()
+    #     # print(labels)
+    #
+    #     input_database = {
+    #         0: X_train_reshaped[:2000, :]
+    #     }
 
-        (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-        d1, d2, d3 = X_train.shape
-        X_train_reshaped = X_train.reshape(d1, d2 * d3)
-        print(X_train_reshaped[:2000, :].shape)
-        y_train_half = y_train[:2000]
-        classes = y_train_half.tolist()
-        labels = y_train_half.tolist()
-        # print(labels)
 
+        input_data = pd.read_csv(filename, header=header)
+        print(input_data.head())
+        classes = input_data[30].tolist()
+        labels = input_data[30].tolist()
+        # 0: input_data.as_matrix([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
         input_database = {
-            0: X_train_reshaped[:2000, :]
+            0: input_data.as_matrix([0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,17,18,19,20,21,22,23,24,25,26,27,28,29])
         }
-
-
-
-        # input_data = pd.read_csv(filename, header=header)
-        # print(input_data.head())
-        # classes = input_data[30].tolist()
-        # labels = input_data[30].tolist()
-        # # 0: input_data.as_matrix([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
-        # input_database = {
-        #     0: input_data.as_matrix([0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,17,18,19,20,21,22,23,24,25,26,27,28,29])
-        # }
 
         return input_database, labels, classes
